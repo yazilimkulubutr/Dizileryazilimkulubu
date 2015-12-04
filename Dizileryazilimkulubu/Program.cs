3b52ded19006fdb797178deb0204ede5de09d63a
yazilimkulubutr <yazilimkulubu@hotmail.com>
2015-12-04 21:58:57 +0200

    Start

---
+++ Dizileryazilimkulubu/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dizileryazilimkulubu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Diller = new string[3];
+            Diller[0] = "C#";
+            Diller[1] = "JavaScript";
+            Diller[2] = "Java";
+  
+            Console.WriteLine(Diller[2]);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
